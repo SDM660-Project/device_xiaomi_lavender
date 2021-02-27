DillerOFire/android_device_xiaomi_lavender

lavender: Move thermal-engine configs from vendor to device tree
Signed-off-by: Subhajeet Muhuri <subhajeet.muhuri@gmail.com>
Change-Id: I4ab10ae9a40b60e7b36f79e3b41861a88b765b21

device.mk
@@ -67,6 +67,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf \
+    $(LOCAL_PATH)/configs/thermal/thermal-engine-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-video.conf
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service

SDM660-Project/device_xiaomi_lavender

Signed-off-by: alanndz <alanmahmud0@gmail.com>
Signed-off-by: sohamxda7 <sensoham135@gmail.com>

device.mk
@@ -60,6 +60,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     libcamera_sdm660_shim
 
+# Thermal
+PRODUCT_COPY_FILES += \
+    $(DEVICE_PATH)/configs/thermal/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf \
+    $(DEVICE_PATH)/configs/thermal/thermal-engine-map.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-map.conf \
+    $(DEVICE_PATH)/configs/thermal/thermal-engine-normal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-normal.conf \
+    $(DEVICE_PATH)/configs/thermal/thermal-engine-video.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine-video.conf
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service