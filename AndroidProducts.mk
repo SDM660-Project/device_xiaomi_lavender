8d840417d4e400cfb0196eedb35d9f46cb7ae0a3
chrisw444 <wandersonrodriguesf1@gmail.com>
2021-06-26 02:14:10 -0300

    lavender: octavi-ify
    Signed-off-by: chrisw444 <wandersonrodriguesf1@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,9 +14,9 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_lavender.mk
+    $(LOCAL_DIR)/octavi_lavender.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_lavender-user \
-    lineage_lavender-userdebug \
-    lineage_lavender-eng
+    octavi_lavender-eng \
+    octavi_lavender-userdebug \
+    octavi_lavender-user
